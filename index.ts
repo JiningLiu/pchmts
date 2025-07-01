510b090244850a322b35eacc88d28347170d5bf4
Jining Liu <hello@jining.dev>
2025-07-01 16:50:03 -0500

    dev0

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
+const pchmtsMkfifoProc = Bun.spawn(["mkfifo", "/tmp/pchm.ts"]);
+await pchmtsMkfifoProc.exited;
+
 Bun.serve({
   port: 80,
   routes: {
     "/stream": () => {
-      const file = Bun.file("pchm.ts");
+      const file = Bun.file("/tmp/pchm.ts");
+
       return new Response(file.stream(), {
         headers: {
           "Content-Type": "video/mp2t",
