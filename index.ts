e0e72f1b82bdbdbf20b134c790cd3d7486cd4a63
Jining Liu <hello@jining.dev>
2025-07-01 23:35:38 -0500

    dev8

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
+const fifoPath = "/tmp/pchm.ts";
+await Bun.spawn(["rm", "-f", fifoPath]).exited;
+await Bun.spawn(["mkfifo", fifoPath]).exited;
+
+const fifoFile = Bun.file(fifoPath);
+const stream = fifoFile.stream();
+
 Bun.serve({
   port: 20240,
 
   routes: {
     "/pchmts": () => {
-      Bun.spawnSync(["pkill", "-f", '"cat /tmp/pchm.ts"']);
-      const file = Bun.file("/tmp/pchm.ts");
-
-      return new Response(file.stream(), {
+      return new Response(stream, {
         headers: {
           "Content-Type": "video/mp2t",
           "Cache-Control": "no-cache",
